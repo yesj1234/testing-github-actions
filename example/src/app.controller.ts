53d672790a94987950d3753f7437b46107b3b8d6
jack <juesc007@gmail.com>
2024-01-22 12:52:24 +0900

    add testCI GET route

--- example/src/app.controller.ts
+++ example/src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get()
+  testCI(): string {
+    return this.appService.getTestCI();
+  }
 }
--- example/src/app.service.ts
+++ example/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getTestCI(): string {
+    return 'Testing github CI';
+  }
 }
